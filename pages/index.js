9fa0b6cd0da47b2e6c3b6b52690ccabf989b1360
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2020-12-04 13:47:44 +0900

    動的ルーティング

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
   return (
@@ -15,7 +17,13 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.list}>
         {allPostsData.map(({ id, date, title }) => (
           <li className={utilStyles.listItem} key={id}>
-            {date} [{id}] {title}
+            <Link href={`posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lisghtText}>
+              <Date dateString={date} />
+            </small>
           </li>
         ))}
       </section>
